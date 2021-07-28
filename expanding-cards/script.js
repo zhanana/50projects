7d831ecc20ab9a11768fad5f1cd29f5d5342b7ad
zhanana <614556279@qq.com>
2021-07-28 12:25:31 +0800

    第一个项目

---
+++ expanding-cards/script.js
@@ -0,0 +1,11 @@
+const panels = document.querySelectorAll('.panel')
+
+
+panels.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        panels.forEach((item)=>{
+            item.classList.remove('active')
+        })
+        item.classList.add('active')
+    })
+})
